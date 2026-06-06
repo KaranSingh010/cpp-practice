87dd66dd28e44744a8b17d7170dd63598a3bb658
KaranSingh010 <karansingh420u@gmail.com>
2026-06-06 20:38:28 +0530

    Create Solid Pyramid.cpp

---
+++ Solid Pyramid.cpp
@@ -0,0 +1,28 @@
+/*
+Input n=5
+
+Output:
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+
+*/
+# include <iostream>
+using namespace std;
+
+int main (){
+    int row,col,n;
+    cout<<"Enter the Input : ";
+    cin>> n;
+    for(row=1;row<=n;row++){
+        for(col=1;col<=n-row;col++){
+            cout<<"  ";
+        }
+        for(col=1;col<=2*row-1;col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
