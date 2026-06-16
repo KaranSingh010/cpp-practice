a9018fe71a3bee1152f1bf223dbc1e41bad8f977
KaranSingh010 <karansingh420u@gmail.com>
2026-06-16 14:56:47 +0530

    Create Bubble Sort .cpp

---
+++ Bubble Sort .cpp
@@ -0,0 +1,37 @@
+/*
+
+Enter the size of array : 5
+Enter the elements : 4 55 3 99 1
+1 3 4 55 99 
+
+*/
+
+# include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int arr[1000];
+    int n;
+    cout<<"Enter the size of array : ";
+    cin>>n;
+    cout<<"Enter the elements : ";
+    for(int i=0;i<n;i++){
+    cin>>arr[i];
+    } 
+  for(int i=n-2;i>=0;i--){
+      bool swapped = 0;
+      for(int j=0;j<=i;j++){
+          
+          if(arr[j]>arr[j+1]){
+              swapped =1;
+              swap(arr[j],arr[j+1]);
+          }
+  }
+   if(swapped==0){
+       break;
+}
+}
+        for(int i=0;i<n;i++)
+              cout<<arr[i]<<" ";
+}
